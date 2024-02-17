6c6c3f026d777bee065fd55408330af0eadd2b93
Aniketh Gaonkar <87714033+offani@users.noreply.github.com>
2024-02-17 14:37:24 +0000

    ff

--- sql_query_generator.py
+++ sql_query_generator.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 import streamlit as st
-from langchain.sql_database import SQLDatabase
+from langchain.sql_database import SQLDatabase 
 from langchain.chains import create_sql_query_chain
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -13,15 +13,15 @@ API_KEY = os.getenv('Google_API_KEY')
 
 genai.configure(api_key=API_KEY)
 
+# db details 
+# username = 'avnadmin'
+# password = 'AVNS_JGYSlUE8_DMQFL0Y4ml'
+# host = 'pg-gemini-server-gem-postgre.a.aivencloud.com'
+# port = '22752'  
+# database_name = 'pg-gemini-server'
 
-username = 'root'
-password = ''
-host = 'localhost'
-port = '3306'  
-database_name = 'police_data'
 
-
-db = SQLDatabase.from_uri("mysql+mysqlconnector://"+username+":"+password+"@"+host+":"+port+"/"+database_name)
+db = SQLDatabase.from_uri("postgres://avnadmin:AVNS_Nq9wk_bgSlbqY2FncEY@pg-gemini-gem-postgre.a.aivencloud.com:22752/defaultdb?sslmode=require")
 
 
 def get_gemini_response(prompt):
